3a42a27b6cb2960da72925d95efef6f4ccda6560
Mathawee <mathaweetj@gmail.com>
2017-05-17 19:47:45 +0700

    Setup

--- config/database.php
+++ config/database.php
@@ -41,12 +41,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3310'),
-            'database' => env('DB_DATABASE', 'sdd2016_2'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'host' => getenv('DB_HOST', 'localhost'),
+            'port' => getenv('DB_PORT', '8889'),
+            'database' => getenv('DB_DATABASE', 'sdd2016_2'),
+            'username' => getenv('DB_USERNAME', 'root'),
+            'password' => getenv('DB_PASSWORD', 'root'),
+            'unix_socket' => getenv('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
