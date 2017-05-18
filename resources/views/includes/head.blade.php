474c2dfc5f977670f4cfe68db82ef60d64f2fe73
ploysaintlouis <ploy.zenlouis@gmail.com>
2017-05-18 23:16:07 +0700

    Add UI

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,7 +1,65 @@
 <meta charset="utf-8">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"></script>
+
+    <!-- BOOTSTRAP CORE STYLE  -->
+    <!-- <link href="assets/css/bootstrap.css" rel="stylesheet" /> -->
+    <link href="<?php echo asset('css/bootstrap.css') ?>" rel="stylesheet" />
+    <!-- FONT AWESOME STYLE  -->
+    <!-- <link href="assets/css/font-awesome.css" rel="stylesheet" /> -->
+    <link href="<?php echo asset('css/font-awesome.css') ?>" rel="stylesheet" />
+    <!-- DATATABLE STYLE  -->
+    <!-- <link href="assets/js/dataTables/dataTables.bootstrap.css" rel="stylesheet" /> -->
+    <link href="<?php echo asset('js/dataTables/dataTables.bootstrap.css') ?>" rel="stylesheet" />
+    <!-- CUSTOM STYLE  -->
+    <!-- <link href="assets/css/style.css" rel="stylesheet" /> -->
+    <link href="<?php echo asset('css/style.css') ?>" rel="stylesheet" />
+    <!-- GOOGLE FONT -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script src="{{asset('js/laravel.js')}}"></script>
-<script>window.csrfToken = '<?php echo csrf_token(); ?>';</script>
+<script>window.csrfToken = '<?php echo csrf_token(); ?>';</script>		
+
+    <div class="navbar navbar-inverse set-radius-zero" >
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">
+                </a>
+            </div>
+            <div class="right-div">
+                <a href="{{ URL::to('logout') }}" class="btn btn-danger pull-right">LOG ME OUT</a>
+            </div>
+        </div>
+    </div>
+    <!-- LOGO HEADER END-->
+    <section class="menu-section">
+        <div class="container">
+            <div class="row ">
+                <div class="col-md-12">
+                    <div class="navbar-collapse collapse ">
+                        <ul id="menu-top" class="nav navbar-nav navbar-right">
+                            <li><a href="index.php" class="menu-top-active">HOME</a></li>
+                           
+                            <li><a href="#">FORMS</a></li>
+                            <li>
+                                <a href="{{ URL::to('getAssignments') }}" class="dropdown-toggle" id="ddlmenuItem" data-toggle="dropdown">ASSIGNMENT<i class="fa fa-angle-down"></i></a>
+                                <ul class="dropdown-menu" role="menu" aria-labelledby="ddlmenuItem">
+                                    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">UI ELEMENTS</a></li>
+                                     <li role="presentation"><a role="menuitem" tabindex="-1" href="#">EXAMPLE LINK</a></li>
+                                </ul>
+                            </li>
+                           <li><a href="{{ URL::to('userLists') }}" >USER MANAGEMENT</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
