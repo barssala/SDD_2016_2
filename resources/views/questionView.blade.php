16f7b6312f6be55d577db2fee9f25212d1eecb20
Ploy-Nannaphat <ploy.zenlouis@gmail.com>
2017-05-20 19:42:44 +0700

    edit UI/move Table of Test case to the right hand side

--- resources/views/questionView.blade.php
+++ resources/views/questionView.blade.php
@@ -68,94 +68,89 @@
     </head>
 
     <body>
-    	<div class="content-wrapper">
-        	<div class="container">
-					<fieldset>
-						<div class="col-md-6">
-				            <div class="panel panel-info">
-				                <div class="panel-heading">
-				                    Question [Edit]
-				                </div> 
-				                <div class="panel-body">
+	<div class="content-wrapper">
+	<div class="container">
+	    <div class="row">
+	        <div class="col-md-4 col-sm-4 col-xs-12">
+	 			<div class="panel panel-info">
+					<div class="panel-heading">
+						Question [Edit]
+					</div> 
+					<div class="panel-body">
+						<div class="form-group">
+							<label for="assignment">Question Name</label>
+							<div class="controls">
+								{{$question->name}}
+							</div>
+						</div>
 
-					                <div class="form-group">
-	                                	<label for="assignment">Question Name</label>
-	                                	<div class="controls">
-	                                		{{$question->name}}
-	                                	</div>
-	                                </div>
+						<div class="form-group">
+							<label for="description">Description</label>
+							<div class="controls">
+								{{$question->description}}
+							</div>
+						</div>
 
-	                                <div class="form-group">
-		                                <label for="description">Description</label>
-		                                <div class="controls">
-		                                	{{$question->description}}
-		                                </div>
-		                            </div>
+						<div class="form-group">
+							<label for="Guideline">Guideline</label>
+							<div class="controls">
+								{{$question->guideline}}
+							</div>
+						</div>
 
-		                            <div class="form-group">
-		                                <label for="Guideline">Guideline</label>
-		                                <div class="controls">
-		                                	{{$question->guideline}}
-		                                </div>
-		                            </div>
+						<div class="form-group">
+							<label for="score">Score</label>
+							<div class="controls">
+								{{$question->score}}
+							</div>
+						</div>
 
-		                            <div class="form-group">
-	                                	<label for="score">Score</label>
-	                                	<div class="controls">
-	                                		{{$question->score}}
-	                                	</div>
-	                                </div>
+						<div class="form-group">
+							<label for="status">Status</label>
+							<div class="controls">
+								{{ $question->active ? "ACTIVE" : "INACTIVE" }}
+							</div>
+						</div>
 
-	                                <div class="form-group">
-		                                <label for="status">Status</label>
-		                                <div class="controls">
-		                                	{{ $question->active ? "ACTIVE" : "INACTIVE" }}
-			                            </div>
-		                            </div>
-
-		                            <div class="control-group">
-			                            <div class="controls">
-			                            <a href="../viewAssignment/{{ $question->assignment_id }}" class="btn btn-success">Back</a>
-			                            </div>
-			                        </div>
-
-	                            </div>
-			               	</div>
-			            </div>
-					</fieldset>
-			</div>
-		</div>
-
-<?php if ((session('user')->role_id != 3)): ?>
-		<div class="container">
-	        <div class="row">
-	                <div class="col-md-11">
-	                  <!-- Table of Question -->
-	                    <div class="panel panel-default">
-	                        <div class="panel-heading">
-	                        	Table of Test case
-	                        </div>
-	                        <div class="panel-body">
-	                            <div class="table-responsive">
-	                                <table id = "testCase-table" class="table table-striped table-bordered table-hover">
-	                                    <thead>
-	                                        <tr>
-	                                            <th>No.</th>
-	                                            <th>Input</th>
-	                                            <th>Output</th>
-	                                            <?php if ((session('user')->role_id != 3)): ?>
-	                                            	<th></th>
-	                                            <?php endif; ?>
-	                                        </tr>
-	                                    </thead>
-	                                    <tbody></tbody>
-	                               </table>
-	                            </div>
-	                        </div>
-	                    </div>
+						<div class="control-group">
+							<div class="controls">
+								<a href="../viewAssignment/{{ $question->assignment_id }}" class="btn btn-success">Back</a>
+							</div>
+						</div>
 					</div>
-			 </div>
+	     		</div>
+	        </div>
+
+			<div class="col-md-8 col-sm-8 col-xs-12">
+				<div class="panel panel-success">
+					<div class="panel-heading">
+						Table of Test case
+					</div>
+
+					<div class="panel-body">
+						<div class="table-responsive">
+							<table id = "testCase-table" class="table table-striped table-bordered table-hover">
+								<thead>
+								<tr>
+									<th>No.</th>
+									<th>Input</th>
+									<th>Output</th>
+										<?php if ((session('user')->role_id != 3)): ?>
+									<th></th>
+										<?php endif; ?>
+								</tr>
+								</thead>
+								<tbody></tbody>
+							</table>
+						</div>
+					</div>
+
+	            </div>
+	        </div>
+	             
 		</div>
-		<?php endif; ?>
+	</div>
+	</div>
+			@include('includes.footer')
 	</body>
 </html>
