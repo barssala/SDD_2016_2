e896aa5ab845c34638ee3de198ba5ad229300f50
ploysaintlouis <ploy.zenlouis@gmail.com>
2017-05-20 17:58:04 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,10 @@
 <!doctype html>
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta charset="utf-8">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
 <title>Dashboard</title>
 
 @include('includes.head')
